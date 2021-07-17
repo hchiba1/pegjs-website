451689987443ac9dd8d9df49cdd0f8567455a343
Hirokazu Chiba <hchiba1@gmail.com>
2021-07-17 16:10:16 +0900

    modify app.js to simplify app

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
 
-var bodyParser = require("body-parser"),
-    express    = require("express"),
-    layout     = require("express-layout"),
-    logger     = require("morgan");
+const bodyParser = require("body-parser");
+const express = require("express");
+const layout = require("express-layout");
+const logger = require("morgan");
 
-var app = express();
+const PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 10000;
+const IP = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
-/* Configuration */
-
-var PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
-var IP = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-
+const app = express();
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
@@ -19,13 +16,12 @@ app.use(logger("dev"));
 app.use(express.static(__dirname + "/public"));
 
 app.use(layout());
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.locals.req = req;
   next();
 });
 
 /* Helpers */
-
 app.locals.menuItem = function(req, id, title) {
     return "<a"
       + (req.path === "/" + id ? " class=\"current\"" : "")
@@ -34,45 +30,15 @@ app.locals.menuItem = function(req, id, title) {
       + "</a>";
 };
 
-/* Routes */
-
-app.get("/", function(req, res) {
-  res.render("index", { title: "" });
-});
-
-app.get("/online", function(req, res) {
-  res.render("online", { title: "Online version", layout: "layout-online" });
-});
-
-app.post("/online/download", bodyParser.urlencoded({ extended: false, limit: "1024kb" }), function(req, res) {
-  res.set("Content-Type", "application/javascript");
-  res.set("Content-Disposition", "attachment;filename=parser.js");
-  res.send(req.body.source);
-});
-
-app.get("/documentation", function(req, res) {
-  res.render("documentation", { title: "Documentation" });
-});
-
-app.get("/development", function(req, res) {
-  res.render("development", { title: "Development" });
-});
-
-app.get("/download", function(req, res) {
-  res.redirect(301, "/#download");
+app.get('/', (req, res) => {
+  res.render("online", { title: "Validator", layout: "layout-online" });
 });
 
 /* Main */
 
-var server = app.listen(PORT, IP, function() {
-  var host = server.address().address,
-      port = server.address().port,
-      env  = app.get("env");
-
-  console.log(
-    "PEG.js website running at http://%s:%d in %s mode...",
-    host,
-    port,
-    env
-  );
+var server = app.listen(PORT, IP, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  const env = app.get("env");
+  console.log(`PEG.js website running at http://${host}:${port} in ${env} mode...`);
 })
